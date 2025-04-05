f2322ec989b7da0db8b6192a9876c4becc02765c
312Prime <fhemflrh60@gmail.com>
2025-04-06 04:07:23 +0900

    Base Name API set

--- src/main/kotlin/com/example/berry_server/berry_server/controller/NameController.kt
+++ src/main/kotlin/com/example/berry_server/berry_server/controller/NameController.kt
@@ -1,4 +1,28 @@
 package com.example.berry_server.berry_server.controller
 
-class NameController {
+import com.example.berry_server.berry_server.repository.NameRepository
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/name")
+class NameController(
+    private val nameRepository: NameRepository
+) {
+
+    // 이름 생성
+    @PostMapping("/createName")
+    fun createName(@RequestBody name: String): String {
+        nameRepository.createName(name)
+        return "이름 등록: $name"
+    }
+
+    // 이름 전체 조회
+    @GetMapping("/getName")
+    fun getNames(): List<String> {
+        return nameRepository.getNameList()
+    }
 }
--- src/main/kotlin/com/example/berry_server/berry_server/repository/NameRepository.kt
+++ src/main/kotlin/com/example/berry_server/berry_server/repository/NameRepository.kt
@@ -1,4 +1,17 @@
 package com.example.berry_server.berry_server.repository
 
+import org.springframework.stereotype.Repository
+
+@Repository
 class NameRepository {
+
+    private val nameList = mutableListOf<String>()
+
+    fun createName(name: String) {
+        nameList.add(name)
+    }
+
+    fun getNameList(): List<String> {
+        return nameList.toList()
+    }
 }
