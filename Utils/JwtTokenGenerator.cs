a967dc4ddf9ee6b03bdbf48fdde3127cbeef2bf4
312Prime <fhemflrh60@gmail.com>
2025-06-01 11:25:15 +0900

    토큰 의존성 / 주석 추가

--- Utils/JwtTokenGenerator.cs
+++ Utils/JwtTokenGenerator.cs
@@ -15,14 +15,19 @@ namespace blueberry_homework_dotnet.Utils
 
             var claims = new[]
             {
+                // UID
                 new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+                // Email
                 new Claim("email", email),
+                // Name
                 new Claim("name", name),
+                // IAT
                 new Claim(JwtRegisteredClaimNames.Iat, DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64)
             };
 
             var token = new JwtSecurityToken(
                 claims: claims,
+                // Expiration
                 expires: DateTime.UtcNow.AddHours(5),
                 signingCredentials: creds
             );
